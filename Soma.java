5a22afa0be9be1031bcc14539fd009f39d93e0ab
KdAndrade <kauand.andrade17@gmail.com>
2025-05-18 14:59:42 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,12 @@
+public class Soma {
+  public static void main(String[] args) {
+    int A = 3;
+    int B = 5;
+    System.out.println(adicao(A, B));
+  }
+
+  public static int adicao(int x, int y) {
+    int soma = x + y;
+    return soma;
+  }
+}
