2e1701f246c7b98c3d1a27f8ede4d9c88ccd217e
KdAndrade <kauand.andrade17@gmail.com>
2025-05-26 21:19:14 -0300

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Function {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite o número de linhas: ");
+    int num1 = sc.nextInt();
+
+    sc.close();
+
+    linha(num1);
+  }
+
+  public static void linha(int n) {
+    int[][] array = new int[n][];
+
+    for (int i = 0; i < n; i++) {
+      array[i] = new int[i + 1];
+      for (int j = 0; j <= i; j++) {
+        array[i][j] = i + 1;
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < array[i].length; j++) {
+        System.out.print(array[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
