3bf57f48265faa0fa6a3a44e86c7ff6b49967919
KdAndrade <kauand.andrade17@gmail.com>
2025-05-28 20:58:26 -0300

    Soma.java

--- Soma.java
+++ Soma.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Soma {
   public static void main(String[] args) {
-    int A = 3;
-    int B = 5;
-    System.out.println(adicao(A, B));
+    Scanner sc = new Scanner(System.in);
+    int[] numeros = new int[3];
+
+    for (int i = 0; i < 3; i++) {
+      System.out.println("Escreve um número inteiro: ");
+      numeros[i] = sc.nextInt();
+
+    }
+
+    int resultado = adicao(numeros[0], numeros[1], numeros[2]);
+
+    System.out.println("Esse é o resultado: " + resultado);
+
+    sc.close();
   }
 
-  public static int adicao(int x, int y) {
-    int soma = x + y;
+  public static int adicao(int x, int y, int z) {
+    int soma = x + y + z;
     return soma;
   }
 }
